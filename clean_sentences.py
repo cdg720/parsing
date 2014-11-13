9c79a364887395d91a45aa4f8090e26606fa00f7
Do Kook Choe <cdg720@gmail.com>
2014-11-12 19:20:18 -0500

    data_fitting.py

--- clean_sentences.py
+++ clean_sentences.py
@@ -5,17 +5,17 @@ import sys
 # NYT only. check for other coropra.
 def clean_sentences():
 	if len(sys.argv) == 1:
-		print 'usage: python clean_sentences.py file1 file2 ...'
-		print 'e.g.: python clean_sentences.py ~/data/gigaword/nyt/*.gz'
+		print 'usage: python clean_sentences.py file1 file2 ... out'
+		print 'e.g.: python clean_sentences.py ~/data/gigaword/nyt/*.gz /pro/dpg/dc65/data/gigaword/chunks/nyt'
 		sys.exit(0)
 
 	doc_size = 500
-	out = '/home/dc65/Documents/research/self-training/data/afe'
+	out = sys.argv[-1]
 	i = 0
 	g = None
 
 	x = [0,] * 2
-	for doc in sys.argv[1:]:
+	for doc in sys.argv[1:-1]:
 		f = gzip.open(doc, 'rb')
 		y = [0,] * 2
 		for line in f.read().splitlines():
@@ -36,7 +36,7 @@ def clean_sentences():
 					count2 += 1
 			x[1] += 1
 			y[1] += 1
-			if count * 100 / count2 >= 90:
+			if count * 100 / count2 >= 90: # go back to 90
 				x[0] += 1
 				y[0] += 1
 				if i % doc_size == 0: # new file
@@ -48,4 +48,33 @@ def clean_sentences():
 				i += 1
 		print doc, y, x
 
-clean_sentences()
+def clean_sentences2():
+	if len(sys.argv) == 1:
+		print 'usage: python clean_sentences.py file1 file2 ... out'
+		print 'e.g.: python clean_sentences.py ~/data/gigaword/nyt/*.gz /pro/dpg/dc65/data/gigaword/chunks/nyt'
+		sys.exit(0)
+
+	doc_size = 500
+	out = sys.argv[-1]
+	i = 0
+	g = None
+
+	x = [0,] * 2
+	for doc in sys.argv[1:-1]:
+		f = gzip.open(doc, 'rb')
+		y = [0,] * 2
+		for line in f.read().splitlines():
+			x[1] += 1
+			y[1] += 1
+			x[0] += 1
+			y[0] += 1
+			if i % doc_size == 0: # new file
+				if g:
+					g.flush()
+					g.close()
+				g = gzip.open(out + '/' + str(i / doc_size + 1) + '.gz', 'wb')
+			g.write(line + '\n')
+			i += 1
+		print doc, y, x
+
+clean_sentences2()
